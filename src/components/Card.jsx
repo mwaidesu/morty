2f7c48f09ad405b827cc9999c2151e74b372f007
Mwai <machariamwai@gmail.com>
2023-06-28 19:48:32 +0300

    Reduce padding on card descriptions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ const Card = (props) => {
            {props.status}
           </p>
         </div>
-        <div className="px-6 pt-4 pb-2">
+        <div className="px-4">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             {props.origin}
           </span>
